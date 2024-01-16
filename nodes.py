efc1b95f3346fbd4da21a1ab49d083d9263fcc8a
Kijai <40791699+kijai@users.noreply.github.com>
2024-01-16 17:10:07 +0200

    Tiled vae, batch support, progress bar
    Breaks old nodes as they are, should be remade.

--- nodes.py
+++ nodes.py
@@ -26,12 +26,21 @@ class CCSR_Upscale:
             "image": ("IMAGE", ),
             "resize_method": (s.upscale_methods, {"default": "lanczos"}),
             "scale_by": ("FLOAT", {"default": 1.0, "min": 0.01, "max": 20.0, "step": 0.01}),
-            "steps": ("INT", {"default": 45, "min": 2, "max": 4096, "step": 1}),
+            "steps": ("INT", {"default": 45, "min": 3, "max": 4096, "step": 1}),
             "t_max": ("FLOAT", {"default": 0.6667,"min": 0, "max": 1, "step": 0.01}),
             "t_min": ("FLOAT", {"default": 0.3333,"min": 0, "max": 1, "step": 0.01}),
+            "sampling_method": (
+            [   
+                'ccsr',
+                'ccsr_tiled_mixdiff',
+                'ccsr_tiled_vae_gaussian_weights',
+            ], {
+               "default": 'ccsr_tiled_mixdiff'
+            }),
             "tile_size": ("INT", {"default": 512, "min": 1, "max": 4096, "step": 1}),
             "tile_stride": ("INT", {"default": 256, "min": 1, "max": 4096, "step": 1}),
-            "tiled": ("BOOLEAN", {"default": False}),
+            "vae_tile_size_encode": ("INT", {"default": 1024, "min": 2, "max": 4096, "step": 8}),
+            "vae_tile_size_decode": ("INT", {"default": 1024, "min": 2, "max": 4096, "step": 8}),
             "color_fix_type": (
             [   
                 'none',
@@ -41,6 +50,7 @@ class CCSR_Upscale:
                "default": 'adain'
             }),
             "keep_model_loaded": ("BOOLEAN", {"default": False}),
+            
             },
             
             }
@@ -52,12 +62,12 @@ class CCSR_Upscale:
     CATEGORY = "CCSR"
 
     @torch.no_grad()
-    def process(self, ccsr_model, image, resize_method, scale_by, steps, t_max, t_min, tiled,tile_size, tile_stride, color_fix_type, keep_model_loaded):
+    def process(self, ccsr_model, image, resize_method, scale_by, steps, t_max, t_min, tile_size, tile_stride, color_fix_type, keep_model_loaded, vae_tile_size_encode, vae_tile_size_decode, sampling_method):
         comfy.model_management.unload_all_models()
         device = comfy.model_management.get_torch_device()
         config_path = os.path.join(script_directory, "configs/model/ccsr_stage2.yaml")
+        empty_text_embed = torch.load(os.path.join(script_directory, "empty_text_embed.pt"), map_location=device)
         dtype = torch.float16 if comfy.model_management.should_use_fp16() and not comfy.model_management.is_device_mps(device) else torch.float32
-        
         if not hasattr(self, "model") or self.model is None:
             config = OmegaConf.load(config_path)
             self.model = instantiate_from_config(config)
@@ -69,8 +79,9 @@ class CCSR_Upscale:
             self.model.to(device, dtype=dtype)
         sampler = SpacedSampler(self.model, var_type="fixed_small")
 
+        batch_size = image.shape[0]
         image, = ImageScaleBy.upscale(self, image, resize_method, scale_by)
-
+        
         # Assuming 'image' is a PyTorch tensor with shape [B, H, W, C] and you want to resize it.
         B, H, W, C = image.shape
 
@@ -88,34 +99,52 @@ class CCSR_Upscale:
         resized_image = resized_image.to(device)
         strength = 1.0
         self.model.control_scales = [strength] * 13
-        cond_fn = None
+        
         height, width = resized_image.size(-2), resized_image.size(-1)
         shape = (1, 4, height // 8, width // 8)
-        x_T = torch.randn(shape, device=self.model.device, dtype=dtype)
+        x_T = torch.randn(shape, device=self.model.device, dtype=torch.float32)
         autocast_condition = dtype == torch.float16 and not comfy.model_management.is_device_mps(device)
-        with torch.autocast(comfy.model_management.get_autocast_device(device), dtype=dtype) if autocast_condition else nullcontext():
-            if not tiled:
-                samples = sampler.sample_ccsr(
-                    steps=steps, t_max=t_max, t_min=t_min, shape=shape, cond_img=resized_image,
-                    positive_prompt="", negative_prompt="", x_T=x_T,
-                    cfg_scale=1.0, cond_fn=cond_fn,
-                    color_fix_type=color_fix_type
-                )
-            else:
-                samples = sampler.sample_with_mixdiff_ccsr(
-                    tile_size=tile_size, tile_stride=tile_stride,
-                    steps=steps, t_max=t_max, t_min=t_min, shape=shape, cond_img=resized_image,
-                    positive_prompt="", negative_prompt="", x_T=x_T,
-                    cfg_scale=1.0, cond_fn=cond_fn,
-                    color_fix_type=color_fix_type
-                )
+        out = []    
 
+        with torch.autocast(comfy.model_management.get_autocast_device(device), dtype=dtype) if autocast_condition else nullcontext():
+            for i in range(batch_size):
+                
+
+                if sampling_method == 'ccsr_tiled_mixdiff':
+                    print("Using tiled mixdiff")
+                    samples = sampler.sample_with_mixdiff_ccsr(
+                        empty_text_embed, tile_size=tile_size, tile_stride=tile_stride,
+                        steps=steps, t_max=t_max, t_min=t_min, shape=shape, cond_img=resized_image[i].unsqueeze(0),
+                        positive_prompt="", negative_prompt="", x_T=x_T,
+                        cfg_scale=1.0, 
+                        color_fix_type=color_fix_type
+                    )
+                elif sampling_method == 'ccsr_tiled_vae_gaussian_weights':
+                    self.model._init_tiled_vae(encoder_tile_size=vae_tile_size_encode // 8, decoder_tile_size=vae_tile_size_decode // 8)
+                    print("Using gaussian weights")
+                    samples = sampler.sample_with_tile_ccsr(
+                        empty_text_embed, tile_size=tile_size, tile_stride=tile_stride,
+                        steps=steps, t_max=t_max, t_min=t_min, shape=shape, cond_img=resized_image[i].unsqueeze(0),
+                        positive_prompt="", negative_prompt="", x_T=x_T,
+                        cfg_scale=1.0, 
+                        color_fix_type=color_fix_type
+                    )
+                else:
+                    print("no tiling")
+                    samples = sampler.sample_ccsr(
+                        empty_text_embed, steps=steps, t_max=t_max, t_min=t_min, shape=shape, cond_img=resized_image[i].unsqueeze(0),
+                        positive_prompt="", negative_prompt="", x_T=x_T,
+                        cfg_scale=1.0,
+                        color_fix_type=color_fix_type
+                    )
+                out.append(samples.squeeze(0))
+       
         original_height, original_width = H, W  
         processed_height = samples.size(2)
         target_width = int(processed_height * (original_width / original_height))
-
-        resized_back_image, = ImageScale.upscale(self, samples.permute(0, 2, 3, 1).cpu(), "lanczos", target_width, processed_height, crop="disabled")
-
+        out_stacked = torch.stack(out, dim=0).cpu().to(torch.float32).permute(0, 2, 3, 1)
+        resized_back_image, = ImageScale.upscale(self, out_stacked, "lanczos", target_width, processed_height, crop="disabled")
+        
         if not keep_model_loaded:
             self.model = None            
             comfy.model_management.soft_empty_cache()
